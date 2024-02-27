cf0941d6d5314e0a5f850096103690d5d118b131
sergiifedorishin <151446351+sergiy-fedorishin@users.noreply.github.com>
2024-02-27 12:20:45 +0200

    task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,16 @@
-function getElementWidth(content, padding, border) {
-  const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
-  const borderWidth = parseFloat(border);
-
-  return contentWidth + paddingWidth * 2 + borderWidth * 2;
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
+  if (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+console.log(checkForSpam('Latest technology news'));
+console.log(checkForSpam('JavaScript weekly newsletter'));
+console.log(checkForSpam('Get best sale offers now!'));
+console.log(checkForSpam('Amazing SalE, only tonight!'));
+console.log(checkForSpam('Trust me, this is not a spam message'));
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!'));
+console.log(checkForSpam('[SPAM] How to earn fast money?'));
